0c55623960405992679abc43ce917cef831e615f
frstta <g.c.f.c.2.0@gmail.com>
2018-03-14 10:28:28 +0000

    week3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,29 @@
 class BlogsController < ApplicationController
 	def index
+		@blogs = Blog.all
 	end
+	def new
+		@blog = Blog.new
+		# @つけると、対応するviewで使える
+		# ブログモデルと紐付いた（データを入れる）空の箱を定義
+		# インスタンスと言う。
+	end
+	def create
+		blog = Blog.new(blog_params)
+		# ビューから送られてきたデータをblogという空き箱に入れている
+		blog.save
+		redirect_to "/blogs"
+		# 他のviewに移動するメソッド
+	end
+
+	def show
+		@blog = Blog.find(params[:id])
+	end
+
+	private
+		def blog_params
+			params.require(:blog).permit(:title, :body, :category)
+			# viewから送信されたデータはparamsというメソッドの中に入っている
+			# ストロングパラメータは、セキュリティを強固にしているもの
+		end
 end
